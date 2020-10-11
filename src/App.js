c40e852cdb1bb08d3427d3d204120a3329f3fe4c
angelog05 <agarridotobar@gmail.com>
2020-10-10 22:10:17 -0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+
+const SortableItem = SortableElement(({value}) => {
+  return (
+    <li className="list-group-item">
+      <h3>{value.title}</h3>
+      <p>{value.description}</p>
+    </li>
+  );
+});
+
+const Sortablelist = SortableContainer(({items}) => {
+  return (
+    <ul className="list-group">
+      {
+        items.map((value, i) => <SortableItem value={value} key={i} index={i} />)
+      }
+    </ul>
+  );
+});
+
+function SortableComponent () {
+  const [tasks, setTasks] = useState([
+    {title: 'title 1', description: 'desc one'},
+    {title: 'title 2', description: 'desc two'},
+    {title: 'title 3', description: 'desc three'},
+    {title: 'title 4', description: 'desc four'},
+    {title: 'title 5', description: 'desc five'}
+  ]);
+  return (
+    <Sortablelist items={tasks} onSortEnd={onSortEnd} />
+  );
+}
+
+const onSortEnd = ({ oldIndex, newIndex}) => {
+  console.log({oldIndex, newIndex});
+  const taskCopy = [...tasks];
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <SortableComponent />
+        </div>
+      </div>
     </div>
   );
 }
